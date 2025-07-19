a74a2fca6438f0d63f4dc343f3aa7e236361566a
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2025-07-19 19:06:34 +1000

    refactored code to use a dict rather than match case

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
 import json
 
+events_dict = {
+    "CreateEvent": lambda event: print(f"- Created {event["repo"]["name"]}"),
+    "PushEvent": lambda event: print(f"- Pushed {event["payload"]["size"]} commits to {event["repo"]["name"]}"),
+    "WatchEvent": lambda event: print(f"- Viewed {event["repo"]["name"]}")
+}
+
 username = input("Github Username: ")
 url = f"https://api.github.com/users/{username}/events"
 
@@ -11,13 +17,4 @@ with open('reponse.json', "w") as file:
     json.dump(events, file, indent=4)
 
 for event in events:
-    match event["type"]:
-        case "CreateEvent":
-            print(f"- Created {event["repo"]["name"]}")
-        case "PushEvent":
-            print(f"- Pushed {event["payload"]["size"]} commits to {event["repo"]["name"]}")
-        case "WatchEvent":
-            print(f"- Viewed {event["repo"]["name"]}")
-        case _:
-            print(event["type"])
-            print(event["repo"]["name"])
+    events_dict[event["type"]](event)
