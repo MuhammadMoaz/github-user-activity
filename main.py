9aae8949f66202b80928ef2ac44f72c9cef745ec
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2025-07-19 19:21:10 +1000

    expanded dict to have keys for each event type, values are empty for now

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ import json
 events_dict = {
     "CreateEvent": lambda event: print(f"- Created {event["repo"]["name"]}"),
     "PushEvent": lambda event: print(f"- Pushed {event["payload"]["size"]} commits to {event["repo"]["name"]}"),
-    "WatchEvent": lambda event: print(f"- Viewed {event["repo"]["name"]}")
+    "WatchEvent": lambda event: print(f"- Viewed {event["repo"]["name"]}"),
+    "CommitCommentEvent": lambda event: print(""),
+    "DeleteEvent": lambda event: print(""),
+    "ForkEvent": lambda event: print(""),
+    "GollumEvent": lambda event: print(""),
+    "IssueCommentEvent": lambda event: print(""),
+    "IssuesEvent": lambda event: print(""),
+    "MemberEvent": lambda event: print(""),
+    "PublicEvent": lambda event: print(""),
+    "PullRequestEvent": lambda event: print(""),
+    "PullRequestReviewEvent": lambda event: print(""),
+    "PullRequestReviewCommentEvent": lambda event: print(""),
+    "PullRequestReviewThreadEvent": lambda event: print(""),
+    "ReleaseEvent": lambda event: print(""),
+    "SponsorshipEvent": lambda event: print("")
 }
 
 username = input("Github Username: ")
