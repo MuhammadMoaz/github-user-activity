93bb3e7555d2385ba365ee1723a3d0d1bce608d2
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2026-01-09 19:22:47 +1100

    added error handling

--- main.py
+++ main.py
@@ -5,20 +5,8 @@ events_dict = {
     "CreateEvent": lambda event: print(f"- Created {event["repo"]["name"]}"),
     "PushEvent": lambda event: print(f"- Pushed commit to {event["repo"]["name"]}"),
     "WatchEvent": lambda event: print(f"- Viewed {event["repo"]["name"]}"),
-    "CommitCommentEvent": lambda event: print(f"- Commit Comment"),
-    "DeleteEvent": lambda event: print(f"- Deleted ... "),
-    "ForkEvent": lambda event: print(f"- Forked ... "),
-    "GollumEvent": lambda event: print(f"- Gollum ... "),
-    "IssueCommentEvent": lambda event: print(f"- Issued Comment"),
-    "IssuesEvent": lambda event: print(f"- Issued ..."),
-    "MemberEvent": lambda event: print(f"- Member"),
-    "PublicEvent": lambda event: print(f"- Public Event"),
-    "PullRequestEvent": lambda event: print(f"- Sent a Pull Request ..."),
-    "PullRequestReviewEvent": lambda event: print(f"- Reviewed Pull Request"),
-    "PullRequestReviewCommentEvent": lambda event: print(f"- PRRC"),
-    "PullRequestReviewThreadEvent": lambda event: print(f"- PRRT"),
-    "ReleaseEvent": lambda event: print(f"- Released ..."),
-    "SponsorshipEvent": lambda event: print(f"- Sponsored ...")
+    "ForkEvent": lambda event: print(f"- Forked {event["repo"]["name"]}"),
+    "IssuesEvent": lambda event: print(f"- Opened a new issue in {event["repo"]["name"]}"),
 }
 
 username = input("Github Username: ")
@@ -30,8 +18,11 @@ events = response.json()
 with open('reponse.json', "w") as file:
     json.dump(events, file, indent=4)
 
-for event in events:
-    if event["type"] in events_dict:
-        events_dict[event["type"]](event)
-    else:
-        print(f"Unknown Event Type {event["type"]}")
+while events["message"] != "Not Found":
+    for event in events:
+        if event["type"] in events_dict:
+            events_dict[event["type"]](event)
+        else:
+            print(f"Event: {event["type"]}")
+
+print("User Not Found")
